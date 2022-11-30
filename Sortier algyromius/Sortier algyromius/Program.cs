a0699f8e17e98af90cb640a93adf981b1d1d20f4
SeyWon <119429978+SeyWon@users.noreply.github.com>
2022-11-30 16:39:07 +0100

    Add files via upload

---
+++ Sortier algyromius/Sortier algyromius/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortier_algyromius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[11];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                array[i] = rnd.Next(1, 100);
+            }
+            for (int j = 0; j < array.Length; j++)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] > array[i+1])
+                    {
+                        int temp = array[i];
+
+                        array[i] = array[i+1];
+                        array[i+1] = temp;  
+
+                        
+
+                    }
+                   
+                }
+            }
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
