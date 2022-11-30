933764d0f1b4660f552dee79ce87c482179d8268
SeyWon <119429978+SeyWon@users.noreply.github.com>
2022-11-30 16:38:36 +0100

    Add files via upload

---
+++ ROT15/ROT15/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROT15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            // 97 a - 122 z
+
+
+            string choose;
+
+            Console.WriteLine("ROT 15");
+            Console.WriteLine("Möchten sie lesen oder schreiben");
+
+            choose = Console.ReadLine();
+
+
+            if (choose == "schreiben")
+            {
+                write();
+                Console.ReadKey();
+               
+            }
+            else if (choose == "lesen")
+            {
+                read();
+                Console.ReadKey();
+
+            }
+
+        }
+        static void write()
+        {
+
+            string a;
+            Console.Write("was möchten sie schreiben: ");
+            a = Console.ReadLine();
+
+
+            foreach (char b in a)
+            {
+                int c = b + 15;
+
+                if (c >= 97 & c <= 122)
+                {
+                    Console.Write(Convert.ToChar(c));
+
+
+                }
+                else if (c > 122)
+                {
+
+                    Console.Write(Convert.ToChar(c - 122 + 96));
+
+
+                }
+
+
+            }
+        }
+        static void read()
+        {
+
+            string a;
+
+            Console.Write("was möchten sie lesen: ");
+            a = Console.ReadLine();
+
+
+            foreach (char b in a)
+            {
+                int c = b - 15;
+
+                if (c >= 97 & c <= 122)
+                {
+                    Console.Write(Convert.ToChar(c));
+
+                }
+                else if (c < 97)
+                {
+
+                    Console.Write(Convert.ToChar(c + 122 - 96));
+
+
+                }
+
+
+
+
+            }
+        }
+    }
+}
